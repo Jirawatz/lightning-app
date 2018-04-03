04435fef3cf56037b38980f8c35477e3f8c97cfd
siraben <bensiraphob@gmail.com>
2018-04-03 14:59:23 +0700

    Fixup two returns

--- App.js
+++ App.js
@@ -6,58 +6,59 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  AppRegistry,
-      Image,
-  WebView
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    AppRegistry,
+    Image,
+    WebView
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' +
+        'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-      <WebView
-	  source={{uri: 'https://github.com/facebook/react-native'}}
-      style={{marginTop: 20}}
-      />
-	let pic = {
-         uri: 'http://103.41.84.32/images/HeaderLPI.jpg'
+    render() {
+            <WebView
+	source={{uri: 'https://github.com/facebook/react-native'}}
+        style={{marginTop: 20}}
+            />
+	    let pic = {
+                uri: 'http://103.41.84.32/images/HeaderLPI.jpg'
         };
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-	<Text style={styles.version}>
-	  ISB Weather 0.0.1
-      // Javascript here
-	</Text>
-      </View>
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       To get started, edit App.js
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       {instructions}
+    //     </Text>
+    //     <Text style={styles.version}>
+    //       ISB Weather 0.0.1
+    //   // Javascript here
+    //     </Text>
+    //   </View>
+
+    // );
       return (
-     
-          <WebView 
+          
+              <WebView 
           style={styles.WebViewStyle} 
           source={{uri: 'https://Google.com'}} 
           javaScriptEnabled={true}
           domStorageEnabled={true}  />
-    
-	      );
-    );
+              
+      );
   }
 }
 
